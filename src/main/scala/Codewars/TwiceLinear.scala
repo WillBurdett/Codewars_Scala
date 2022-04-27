1e18ffd822b9bd87be9cef7a60fdd978b516e4a1
William Burdett <williamburdett@williams-air.mynet>
2022-04-27 15:53:48 +0100

    ListBuffer added to twicelinear

--- src/main/scala/Codewars/TwiceLinear.scala
+++ src/main/scala/Codewars/TwiceLinear.scala
@@ -1,33 +1,54 @@
 package Codewars
 
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 
 object TwiceLinear extends App{
 
 
+  // 50ms using ListBuffer
+  def dblLinear(n: Int): Int = {
+        var (times2Index, times3Index) = (0,0)
+        @tailrec
+        def step(currentList : ListBuffer[Int]): Int = {
+          val next2 = 2 * currentList(times2Index) + 1
+          val next3 = 3 * currentList(times3Index) + 1
+
+          if (currentList.length > n) {
+            currentList(n)
+          } else if (next2 <= next3) {
+            times2Index += 1
+            step((currentList :+ next2).distinct)
+          } else {
+            times3Index += 1
+            step((currentList :+ next3).distinct)
+          }
+        }
+        step(ListBuffer(1))
+      }
+
   // TAIL-RECURSION METHOD
 
-  def dblLinear(n: Int): Int = {
-    var (times2Index, times3Index) = (0,0)
-
-    @tailrec
-    def step(currentList : Vector[Int]): Int = {
-
-      val next2 = 2 * currentList(times2Index) + 1
-      val next3 = 3 * currentList(times3Index) + 1
-
-      if (currentList.length > n) {
-        currentList(n)
-      } else if (next2 <= next3) {
-        times2Index += 1
-        step((currentList :+ next2).distinct)
-      } else {
-        times3Index += 1
-        step((currentList :+ next3).distinct)
-      }
-    }
-    step(Vector(1))
-  }
+//  def dblLinear(n: Int): Int = {
+//    var (times2Index, times3Index) = (0,0)
+//    @tailrec
+//    def step(currentList : Vector[Int]): Int = {
+//
+//      val next2 = 2 * currentList(times2Index) + 1
+//      val next3 = 3 * currentList(times3Index) + 1
+//
+//      if (currentList.length > n) {
+//        currentList(n)
+//      } else if (next2 <= next3) {
+//        times2Index += 1
+//        step((currentList :+ next2).distinct)
+//      } else {
+//        times3Index += 1
+//        step((currentList :+ next3).distinct)
+//      }
+//    }
+//    step(Vector(1))
+//  }
 
 
 
